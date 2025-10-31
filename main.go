0742c7790632021ee4effe80513dd7d78896860a
Raoned <raoned99@gmail.com>
2025-10-31 19:25:23 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "github.com/Raoned/goopportunities/router"
+import (
+	"github.com/Raoned/goopportunities/config"
+	"github.com/Raoned/goopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	//Initialize config
+	err := config.Init
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
 	//Initialize router - maiusculo variavel global e minusculo local
 	router.Initialize()
+
 }
